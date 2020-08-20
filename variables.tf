0c1a87e097c8319447d9764d291f0e07356a2653
Deus Eks <jamesnyika@users.noreply.github.com>
2020-08-20 10:49:31 -0400

    Create variables.tf
    Initial commit

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "instance_tenancy" {
+  description = "A tenancy option for instances launched into the VPC."
+  default     = "default"
+}
+
+variable "region" {
+  description = "My provisioning region"
+  default     = "us-east-1"
+}
